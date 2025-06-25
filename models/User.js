cdd2dfbe4d964bf1913046c693c4002a889e3cb9
andrezoficial <andres1997rez@gmail.com>
2025-06-25 00:19:08 -0500

    actualizacion de roles

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  rol: { type: String, enum: ["admin", "usuario"], default: "usuario" },
+  rol: { type: String, enum: ["Administrador", "Editor", "Lector"], default: "Lector" },
   password: { type: String, required: true },
 });
 
