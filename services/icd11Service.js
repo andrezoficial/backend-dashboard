4b84171b3ed447482de441501777fe82f8ae4168
andrezoficial <andres1997rez@gmail.com>
2025-07-18 22:25:13 -0500

    se actualiza historia clinica con api cie-11

--- services/icd11Service.js
+++ services/icd11Service.js
@@ -1,26 +1,42 @@
 // services/icd11Service.js
-const axios       = require('axios');
-const { getToken }= require('./authService');
+const axios = require('axios');
+const https = require('https');
 
 const BASE          = 'https://icd.who.int/icdapi';
 const LINEARIZATION = 'mms';
 const LANGUAGE      = 'es';
 const API_VERSION   = 'v2';
 
+// Creamos un agente que desactiva la validación de certificados
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false
+});
+
 async function buscarICD11(termino) {
   if (!termino) return [];
 
-  const token = await getToken();
-  const url   = `${BASE}/${LINEARIZATION}/search`;
+  const url = `${BASE}/${LINEARIZATION}/search`;
   const params = { q: termino, language: LANGUAGE };
   const headers = {
-    'API-Version':   API_VERSION,
-    'Accept':        'application/json',
-    'Authorization': `Bearer ${token}`
+    'API-Version': API_VERSION,
+    Accept:        'application/json'
   };
 
-  const { data } = await axios.get(url, { params, headers });
-  return data.destinationEntities || [];
+  try {
+    const { data } = await axios.get(url, {
+      params,
+      headers,
+      httpsAgent    // ← aquí le pasamos el agente
+    });
+    return data.destinationEntities || [];
+  } catch (err) {
+    console.error(
+      'ICD-11 API error:',
+      err.response?.status,
+      err.response?.data || err.message
+    );
+    throw err;
+  }
 }
 
 module.exports = { buscarICD11 };
